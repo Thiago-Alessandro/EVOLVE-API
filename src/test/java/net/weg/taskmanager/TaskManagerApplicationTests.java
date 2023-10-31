d69e18a1688468e65721cd0cd4b8e3de4ac92c9d
Thiago Alessandro Batista <alebatistathiago@gmail.com>
2023-10-31 19:01:27 -0300

    First commit

---
+++ src/test/java/net/weg/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.weg.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
