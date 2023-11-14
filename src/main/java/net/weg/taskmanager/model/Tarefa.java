06adcdecf5d143e5862b831b9603e352b6b8fea9
Thiago Alessandro Batista <alebatistathiago@gmail.com>
2023-11-13 22:01:16 -0300

    Testes

--- src/main/java/net/weg/taskmanager/model/Tarefa.java
+++ src/main/java/net/weg/taskmanager/model/Tarefa.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 @Entity
