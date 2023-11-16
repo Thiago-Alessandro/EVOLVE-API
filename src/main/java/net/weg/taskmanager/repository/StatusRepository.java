5b65dd96f6c07417c6d5cf894b616b3b5c2a505a
Thiago Alessandro Batista <alebatistathiago@gmail.com>
2023-11-16 14:32:09 -0300

    implementa novos repositorios

---
+++ src/main/java/net/weg/taskmanager/repository/StatusRepository.java
@@ -0,0 +1,9 @@
+package net.weg.taskmanager.repository;
+
+import net.weg.taskmanager.model.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StatusRepository extends JpaRepository<Status, Integer> {
+}
