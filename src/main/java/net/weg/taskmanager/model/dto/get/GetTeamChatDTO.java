969ccee216b4b13dd4820755c933e4e4fc5aff65
Thiago Alessandro Batista <tbatista@weg.net>
2024-03-26 21:55:58 -0300

    refactor: new method to upload message status

--- src/main/java/net/weg/taskmanager/model/dto/get/GetTeamChatDTO.java
+++ src/main/java/net/weg/taskmanager/model/dto/get/GetTeamChatDTO.java
@@ -1,15 +1,21 @@
 package net.weg.taskmanager.model.dto.get;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import net.weg.taskmanager.model.dto.shortDTOs.TeamShortDTO;
 import net.weg.taskmanager.model.dto.utils.DTOUtils;
 import net.weg.taskmanager.model.entity.TeamChat;
 import org.springframework.beans.BeanUtils;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetTeamChatDTO extends GetChatDTO{
-    private TeamShortDTO getTeamDTO;
+    private TeamShortDTO team;
     public GetTeamChatDTO(TeamChat teamChat){
         super(teamChat);
-        this.getTeamDTO = DTOUtils.teamToShortTeamDTO(teamChat.getTeam());
+        this.team = DTOUtils.teamToShortTeamDTO(teamChat.getTeam());
     }
 
 }
