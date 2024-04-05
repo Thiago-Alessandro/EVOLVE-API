3b4edb1072a3060a69625b2b462dabe345caed9a
Thiago Alessandro Batista <tbatista@weg.net>
2024-04-05 16:34:37 -0300

    refactor: all files in packages except main aplication

--- src/main/java/net/weg/taskmanager/config/CorsConfig.java
+++ src/main/java/net/weg/taskmanager/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package net.weg.taskmanager;
+package net.weg.taskmanager.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
