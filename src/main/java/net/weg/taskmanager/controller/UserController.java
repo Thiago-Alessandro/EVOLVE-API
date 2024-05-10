9ddce888bdb5f0091b0d21b9c8b603a5e42bb3e2
Thiago Batista <tbatista@weg.net>
2024-05-10 19:03:48 -0300

    fix: returns response entity on auth/login

--- src/main/java/net/weg/taskmanager/controller/UserController.java
+++ src/main/java/net/weg/taskmanager/controller/UserController.java
@@ -34,12 +34,12 @@ public class UserController {
     public Collection<GetUserDTO> findAll(){return userService.findAll();}
 
     @PostMapping
-    public GetUserDTO create(@RequestBody PostUserDTO user){
-//        try {
-            return (userService.create(user));
-//        }catch (Exception e){
-//            return new ResponseEntity<>(HttpStatus.CONFLICT);
-//        }
+    public ResponseEntity<GetUserDTO> create(@RequestBody PostUserDTO user){
+        try {
+            return ResponseEntity.ok(userService.create(user));
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
 
     @GetMapping("/login/{email}")
